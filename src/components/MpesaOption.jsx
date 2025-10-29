c90d9d925c1f2ac4d8d17448fe50662738957ac5
Ariel-joe <arieljoe00@gmail.com>
2025-10-29 22:48:41 +0300

    update on the font to Alans

--- src/components/MpesaOption.jsx
+++ src/components/MpesaOption.jsx
@@ -4,16 +4,6 @@ const MpesaOption = () => {
     return (
         <>
             <label className="block text-sm text-neutral-700 mb-2">
-                Amount
-            </label>
-            <div className="relative mb-6">
-                <input
-                    type="number"
-                    className="w-full pl-8 pr-4 py-3 border border-neutral-300 rounded-lg focus:border-neutral-900 focus:outline-none"
-                    placeholder="ksh"
-                />
-            </div>
-            <label className="block text-sm text-neutral-700 mb-2">
                 Phone Number
             </label>
             <div className="relative mb-6">
@@ -21,6 +11,16 @@ const MpesaOption = () => {
                     type="Mpesa"
                     className="w-full pl-8 pr-4 py-3 border border-neutral-300 rounded-lg focus:border-neutral-900 focus:outline-none"
                     placeholder="07..."
+                />
+            </div>
+            <label className="block text-sm text-neutral-700 mb-2">
+                Amount
+            </label>
+            <div className="relative mb-6">
+                <input
+                    type="number"
+                    className="w-full pl-8 pr-4 py-3 border border-neutral-300 rounded-lg focus:border-neutral-900 focus:outline-none"
+                    placeholder="ksh"
                 />
             </div>
             <button class="w-full bg-neutral-900 text-white py-4 rounded-lg hover:bg-neutral-800 transition-colors flex items-center justify-center">
