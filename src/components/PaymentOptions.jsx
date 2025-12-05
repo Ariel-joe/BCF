3cfc7674f70f2e800a54745e74d38f580aae6d49
Ariel-joe <arieljoe00@gmail.com>
2025-12-05 15:40:47 +0300

    minor update

--- src/components/PaymentOptions.jsx
+++ src/components/PaymentOptions.jsx
@@ -17,20 +17,20 @@ const PaymentOptions = () => {
                         <img
                             src={"/logos/mobile-money.png"}
                             alt="mpesa logo"
-                            className="h-6 w-full"
+                            className="h-7 w-full"
                         />
                         <img
                             src={"/logos/visa-mastercard.png"}
                             alt="mpesa logo"
-                            className="h-6 w-full"
+                            className="h-7 w-full"
                         />
                         <img
                             src={"/logos/american-express.png"}
                             alt="mpesa logo"
-                            className="h-6 w-full"
+                            className="h-7 w-full"
                         />
                     </Tab>
-                    <Tab
+                    {/* <Tab
                         className={
                             "flex items-center px-6 py-3 text-neutral-900 data-selected:border-b-2 data-selected:border-neutral-900 bg-white"
                         }
@@ -40,17 +40,17 @@ const PaymentOptions = () => {
                             alt="paypal logo"
                             className="h-5 w-5 mr-2"
                         />
-                    </Tab>
+                    </Tab> */}
                 </TabList>
                 <TabPanels>
                     <TabPanel>
                         <PaystackDonation />
                     </TabPanel>
-                    <TabPanel>
-                        {/* <PaypalOption /> */}
+                    {/* <TabPanel>
+                        <PaypalOption />
 
                         <p>currently not available</p>
-                    </TabPanel>
+                    </TabPanel> */}
                 </TabPanels>
             </TabGroup>
         </>
